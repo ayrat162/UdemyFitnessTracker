50f919979e57aebefd91eb6a98421b2b7f82c4f3
Airat Musin <Airat.Musin@icl-services.com>
2021-05-19 17:51:39 +0300

    section 2 is done

--- my-first-angular-project-v12/src/app/products/products.component.ts
+++ my-first-angular-project-v12/src/app/products/products.component.ts
@@ -1,27 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { ProductsService } from '../services/products.service';
+
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnInit, OnDestroy {
   productName = 'A Book';
   products = ['A Book', 'A Tree'];
+  private productsSubscription: Subscription 
 
-  constructor() { }
+  constructor(private productsService: ProductsService) {
+    this.productsSubscription = this.productsService.productsUpdated.subscribe(() => {
+      this.products = this.productsService.getProducts();
+    });
+   }
 
   ngOnInit(): void {
+    this.products = this.productsService.getProducts();
   }
 
   onAddProduct(form: NgForm): void {
-    if(form.valid){
-      this.products.push(form.value.productName);
+    if (form.valid) {
+      this.productsService.addProduct(form.value.productName);
     }
   }
 
-  onRemoveProduct(productName: string): void {
-    this.products = this.products.filter(p => p !== productName);
+  ngOnDestroy(){
+    this.productsSubscription.unsubscribe();
   }
 }
