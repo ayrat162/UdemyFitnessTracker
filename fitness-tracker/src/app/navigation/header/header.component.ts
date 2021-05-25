51c802ae81e3042f6ba794da8b31d0b76edefb2e
Airat Musin <Airat.Musin@icl-services.com>
2021-05-25 16:40:06 +0300

    lessons [56-60]

--- fitness-tracker/src/app/navigation/header/header.component.ts
+++ fitness-tracker/src/app/navigation/header/header.component.ts
@@ -1,20 +1,30 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   @Output() sidenavToggle = new EventEmitter<void>();
+  isAuth!: boolean;
+  authSubscription!: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
+  }
 
   ngOnInit(): void {
+    this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
   }
 
   onToggleSidenav(): void {
     this.sidenavToggle.emit();
-
   }
 }
